59ca4f271e1b05289ac5fc4f4b532c7a3f36cefb
Injourn <thatguywhowas@live.com>
2022-06-28 17:37:32 -0700

    Add Validation filter.

---
+++ PsuedoMediaBackend/Filters/PsuedoMediaActionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using PsuedoMediaBackend.Models.ProtocolMessages;
+using PsuedoMediaBackend.Services;
+
+namespace PsuedoMediaBackend.Filters {
+    public class PsuedoMediaActionFilter : IActionFilter {
+        AuthenticationService? _authenticationService;
+        public void OnActionExecuted(ActionExecutedContext context) {
+            
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context) {
+            object body = context?.ActionArguments["body"];
+            if (bods is IValidation) {
+                IValidation? validationModel = context?.ActionArguments["body"] as IValidation;
+                _authenticationService = context.HttpContext.Request.HttpContext.RequestServices.GetService<AuthenticationService>();
+                if (validationModel != null) {
+                    validationModel.Validate(_authenticationService);
+                }
+            }
+        }
+    }
+}
--- PsuedoMediaBackend/Program.cs
+++ PsuedoMediaBackend/Program.cs
@@ -13,7 +13,9 @@ builder.Services.AddCors(options => {
     });
 });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => {
+    options.Filters.Add<PsuedoMediaActionFilter>();
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen( opt => {
